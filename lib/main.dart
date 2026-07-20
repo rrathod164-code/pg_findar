cabc31fdae9e296ad5e22b68b3d6e32e85784ae1
jeet <jeetkhunt7874@gmail.com>
2026-07-20 13:15:22 +0530

    changed by jeet

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const Introscreen());
+    return MaterialApp(
+      home: const Introscreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
