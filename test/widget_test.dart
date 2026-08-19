23816489a1cae8b731ad22bb458109eb2c4e2c79
jeet <jeetkhunt7874@gmail.com>
2026-08-19 08:52:00 +0530

    feat: add initial counter smoke test for PGFinderApp widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pg_findar/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PGFinderApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
