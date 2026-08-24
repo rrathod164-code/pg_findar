85ac9b053c1ad02b2ec2d89484f6cc390d73df25
jeet <jeetkhunt7874@gmail.com>
2026-08-24 18:07:46 +0530

    add introduction screen with assets and update asset configuration

--- lib/intro.dart
+++ lib/intro.dart
@@ -3,7 +3,6 @@ import 'package:pg_findar/login.dart';
 
 class Introduction extends StatelessWidget {
   const Introduction({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,64 +24,10 @@ class Introduction extends StatelessWidget {
               // PG Illustration
               Expanded(
                 child: Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.03,
-                      left: MediaQuery.of(context).size.width * 0.03,
-                      right: MediaQuery.of(context).size.width * 0.03,
-                    ),
-                    child: Image.asset(
-                      'assets/images/intro.png',
-                      fit: BoxFit.contain,
-                      width: double.infinity,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-// Next Page
-class NextPage extends StatelessWidget {
-  const NextPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFFF1FBFA), Color(0xFFB9F2E9)],
-          ),
-        ),
-
-        child: SafeArea(
-          child: Column(
-            children: [
-              // PG Illustration
-              Expanded(
-                child: Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.03,
-                      left: MediaQuery.of(context).size.width * 0.03,
-                      right: MediaQuery.of(context).size.width * 0.03,
-                    ),
-                    child: Image.asset(
-                      'assets/images/intro.png',
-                      fit: BoxFit.contain,
-                      width: double.infinity,
-                    ),
+                  child: Image.asset(
+                    'assets/intro.png',
+                    fit: BoxFit.contain,
+                    width: double.infinity,
                   ),
                 ),
               ),
@@ -102,7 +47,7 @@ class NextPage extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => const NextPage(),
+                          builder: (context) => const login_screen(),
                         ),
                       );
                     },
